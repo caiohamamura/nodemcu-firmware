c5def8feb71bf7401429bff706e855cce7deee5b
caiohamamura <caiohamamura@gmail.com>
2025-09-21 00:05:03 -0300

    Use external_win scripts and internal (already cloned repository)

--- tools/02_setup_environment.bat
+++ tools/02_setup_environment.bat
@@ -1,7 +1,6 @@
 Miniforge3-Windows-x86_64.exe /S
 call C:\ProgramData\miniforge3\Scripts\activate.bat
-mamba create -n esp cmake ninja pyserial gcc git -y 
+mamba create -n esp cmake ninja pyserial gcc -y 
 call conda activate esp
-git clone https://github.com/caiohamamura/nodemcu-firmware.git --depth=1 --recurse-submodules --shallow-submodules --branch cmake
 pushd nodemcu-firmware
 call tools\03_configure_build.bat
