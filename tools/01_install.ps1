6e14b616b019857d48527209715e5c78c139395a
caiohamamura <caiohamamura@gmail.com>
2025-09-20 23:51:28 -0300

    Do not display progress in powershell because it slows down terribly.

--- tools/01_install.ps1
+++ tools/01_install.ps1
@@ -1,7 +1,9 @@
-# Run this with Invoke-Expression ((Invoke-WebRequest -Uri https://raw.githubusercontent.com/caiohamamura/nodemcu-firmware/refs/heads/cmake/tools/01_install.ps1 -UseBasicParsing).Content)
+# Run this with $ProgressPreference = 'SilentlyContinue';Invoke-Expression ((Invoke-WebRequest -Uri https://raw.githubusercontent.com/caiohamamura/nodemcu-firmware/refs/heads/cmake/tools/01_install.ps1 -UseBasicParsing).Content);$ProgressPreference = 'Continue'
 mkdir nodemcu_build
 cd nodemcu_build
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/caiohamamura/nodemcu-firmware/refs/heads/cmake/tools/02_setup_environment.bat -UseBasicParsing -OutFile 02_setup_environment.bat
 Invoke-WebRequest -OutFile Miniforge3-Windows-x86_64.exe -Uri "https://github.com/conda-forge/miniforge/releases/download/25.3.1-0/Miniforge3-Windows-x86_64.exe"
+$ProgressPreference = 'Continue'
 cmd /c "02_setup_environment.bat"
 cd nodemcu-firmware
